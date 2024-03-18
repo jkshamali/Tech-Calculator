32701220e1d82b0bf14d87d623f89aea9e83a98a
jkshamali <jkshamali2001@gmail.com>
2024-03-18 10:55:26 +0300

    add new line

--- Calculator/Model/CalculatorButtons.swift
+++ Calculator/Model/CalculatorButtons.swift
@@ -31,7 +31,7 @@ enum CalculatorButtons: String {
     var buttonColor: Color {
         switch self {
         case .add, .subtract, .divide, .multiply, .equal :
-            return .red
+            return .orange
             
         case .clear, .negative, .percent:
             return Color(.lightGray)
