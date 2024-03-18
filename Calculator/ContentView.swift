993ca2ce2b4f3662ff1922cb9b4245c7a4cc672d
jkshamali <jkshamali2001@gmail.com>
2024-03-18 10:50:32 +0300

    first commit

---
+++ Calculator/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  Calculator
+//
+//  Created by dvp on 14/03/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+       Home()
+    }
+}
+
+#Preview {
+    ContentView()
+}
